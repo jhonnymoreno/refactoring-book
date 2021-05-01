c2267562a915b9ec8d0599e48e752c6ea2f1d7d0
jmoreno <jmoreno@ciandt.com>
2021-05-01 20:11:24 -0300

    Applying - Extract Function - (pg 134)

--- RefactoringBook.js
+++ RefactoringBook.js
@@ -32,25 +32,7 @@ let fStatement = function statement(invoice, plays){
 
     for(let perf of invoice.performances) {
         const play = plays[perf.playID];
-        let thisAmount = 0;
-
-        switch(play.type) {
-            case "tragedy":
-                thisAmount = 40000;
-                if(perf.audience > 30) {
-                    thisAmount += 1000 * (perf.audience -30);
-                }
-                break;
-            case "comedy":
-                thisAmount = 30000;
-                if(perf.audience > 20) {
-                    thisAmount += 10000 + 500 * (perf.audience - 20);
-                }
-                thisAmount += 300 * perf.audience;
-                break;
-            default:
-                throw new Error(`unknown type: ${play.type}`);
-        }
+        let thisAmount = amountFor(perf, play);
 
         //soma créditos por volume
         volumeCredits += Math.max(perf.audience - 30, 0);
@@ -67,4 +49,28 @@ let fStatement = function statement(invoice, plays){
     result += `You earned ${volumeCredits} credits \n `;
 
     return result;
+}
+
+function amountFor(perf, play) {
+    let thisAmount = 0;
+
+    switch(play.type) {
+        case "tragedy":
+            thisAmount = 40000;
+            if(perf.audience > 30) {
+                thisAmount += 1000 * (perf.audience -30);
+            }
+            break;
+        case "comedy":
+            thisAmount = 30000;
+            if(perf.audience > 20) {
+                thisAmount += 10000 + 500 * (perf.audience - 20);
+            }
+            thisAmount += 300 * perf.audience;
+            break;
+        default:
+            throw new Error(`unknown type: ${play.type}`);
+    }
+
+    return thisAmount;
 }
