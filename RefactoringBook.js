e282188c69277608cd1d6763c462e1731ce1b68e
jmoreno <jmoreno@ciandt.com>
2021-06-05 20:32:14 -0300

    Using statementData to load customer info and using on renderPlainText

--- RefactoringBook.js
+++ RefactoringBook.js
@@ -26,11 +26,12 @@ let invoice = {
 
 let fStatement = function statement(invoice, plays){
     const statementData = {};
+    statementData.customer = invoice.customer;
     return renderPlainText(statementData, invoice, plays);
 }
 
 function renderPlainText(data, invoice, plays) {
-    let result = `Statement for ${invoice.customer} \n `;
+    let result = `Statement for ${data.customer} \n `;
     for(let perf of invoice.performances) {
         //Exibe a linha para esta requisição
         result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats) \n `
